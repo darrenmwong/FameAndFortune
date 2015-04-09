8c3330fd1c9ce56856857167d4492a0ff5f7eb1b
darrenmwong <darrenwong06@gmail.com>
2015-04-09 16:33:38 -0700

    arrange it like a popout menu

--- app/src/main.js
+++ app/src/main.js
@@ -1,14 +1,3 @@
-/**
- * FlexibleLayout
- * ------------
- *
- * FlexibleLayout is a component for making ratio based layouts
- * similar to HTML5 Flexbox.
- *
- * In this example, use a FlexibleLayout to arrange an array of
- * renderables based on both the parent size and the defined
- * size of the renderables
- */
 define(function(require, exports, module) {
     var Engine = require('famous/core/Engine');
     var Surface = require('famous/core/Surface');
@@ -18,26 +7,20 @@ define(function(require, exports, module) {
     var mainContext = Engine.createContext();
 
     var colors = [
-        'rgba(256, 0, 0, .7)',
-        'rgba(0, 256, 0, .7)',
-        'rgba(0, 0, 256, .7)',
-        'rgba(256, 0, 0, .7)',
-        'rgba(0, 256, 0, .7)',
-        'rgba(0, 0, 256, .7)',
-        'rgba(256, 0, 0, .7)',
-        'rgba(0, 256, 0, .7)',
-        'rgba(0, 0, 256, .7)'
+        'lightgrey',
+        'black',
+
     ];
 
-    var initialRatios = [1, true, 1, true, 1, true, 1, true];
+    var initialRatios = [0, 1];
 
     var flex = new FlexibleLayout({
         ratios : initialRatios
     });
 
     var surfaces = [];
-    for (var i = 1; i <= 8; i++) {
-        size = (i % 2 === 0) ? [10, undefined] : [undefined, undefined]
+    for (var i = 1; i < 3; i++) {
+        size = [undefined, undefined]
         surfaces.push(new Surface({
             size: size,
             properties: {
@@ -48,7 +31,7 @@ define(function(require, exports, module) {
 
     flex.sequenceFrom(surfaces);
 
-    var finalRatios = [4, true, 1, true, 0, true, 7, true];
+    var finalRatios = [3, 7];
     var toggle = false;
     Engine.on('click', function(){
         var ratios = toggle ? initialRatios : finalRatios;
