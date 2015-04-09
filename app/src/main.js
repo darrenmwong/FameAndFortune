01a149e62aca36e723e39a1968918f7f01eb3f61
darrenmwong <darrenwong06@gmail.com>
2015-04-09 16:22:09 -0700

    flexible layout

--- app/src/main.js
+++ app/src/main.js
@@ -1,32 +1,60 @@
-/* globals define */
+/**
+ * FlexibleLayout
+ * ------------
+ *
+ * FlexibleLayout is a component for making ratio based layouts
+ * similar to HTML5 Flexbox.
+ *
+ * In this example, use a FlexibleLayout to arrange an array of
+ * renderables based on both the parent size and the defined
+ * size of the renderables
+ */
 define(function(require, exports, module) {
-    'use strict';
-    // import dependencies
     var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
     var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var FlexibleLayout = require('famous/views/FlexibleLayout');
 
-    // create the main context
     var mainContext = Engine.createContext();
 
-    // your app here
-    mainContext.setPerspective(1000);
+    var colors = [
+        'rgba(256, 0, 0, .7)',
+        'rgba(0, 256, 0, .7)',
+        'rgba(0, 0, 256, .7)',
+        'rgba(256, 0, 0, .7)',
+        'rgba(0, 256, 0, .7)',
+        'rgba(0, 0, 256, .7)',
+        'rgba(256, 0, 0, .7)',
+        'rgba(0, 256, 0, .7)',
+        'rgba(0, 0, 256, .7)'
+    ];
 
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png',
-        classes: ['backfaceVisibility']
+    var initialRatios = [1, true, 1, true, 1, true, 1, true];
+
+    var flex = new FlexibleLayout({
+        ratios : initialRatios
     });
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        align: [0.5, 0.5],
-        origin: [0.5, 0.5],
-        transform: function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
+    var surfaces = [];
+    for (var i = 1; i <= 8; i++) {
+        size = (i % 2 === 0) ? [10, undefined] : [undefined, undefined]
+        surfaces.push(new Surface({
+            size: size,
+            properties: {
+                backgroundColor: colors[i-1]
+            }
+        }));
+    }
+
+    flex.sequenceFrom(surfaces);
+
+    var finalRatios = [4, true, 1, true, 0, true, 7, true];
+    var toggle = false;
+    Engine.on('click', function(){
+        var ratios = toggle ? initialRatios : finalRatios;
+        flex.setRatios(ratios, {curve : 'easeOut', duration : 500});
+        toggle = !toggle;
     });
 
-    mainContext.add(centerSpinModifier).add(logo);
+    mainContext.add(flex);
 });
