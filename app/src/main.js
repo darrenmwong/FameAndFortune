4ba8cb82c58a0b854aa228c4d35b9e0da74775a1
darrenmwong <darrenwong06@gmail.com>
2015-04-14 14:37:05 -0700

    matrix

--- app/src/main.js
+++ app/src/main.js
@@ -1,44 +1,39 @@
 define(function(require, exports, module) {
-    var Engine = require('famous/core/Engine');
-    var Surface = require('famous/core/Surface');
-    var Modifier = require('famous/core/Modifier');
-    var FlexibleLayout = require('famous/views/FlexibleLayout');
+    var Engine     = require("famous/core/Engine");
+    var Surface    = require("famous/core/Surface");
+    var GridLayout = require("famous/views/GridLayout");
 
     var mainContext = Engine.createContext();
 
-    var colors = [
-        'lightgrey',
-        'red'
-    ];
+    var contextSize = mainContext.getSize();
 
-    var initialRatios = [0, 1];
+    var dimensions;
 
-    var flex = new FlexibleLayout({
-        ratios: initialRatios
+    if (contextSize[0] < 480 || contextSize[1] < 480) {
+        dimensions = [2,8];
+    } else {
+        dimensions = [8,2];
+    };
+    
+    var grid = new GridLayout({
+        dimensions: dimensions
     });
 
     var surfaces = [];
-    for (var i = 1; i < 3; i++) {
-        var size = [undefined, undefined];
+    grid.sequenceFrom(surfaces);
+
+    for(var i = 0; i < 8; i++) {
         surfaces.push(new Surface({
-            size: size,
-            content: '<h1>Surface ' + i + '</h1>',
+            content: "I am panel " + (i + 1),
+            size: [undefined, undefined],
             properties: {
-                backgroundColor: colors[i-1],
+                backgroundColor: "hsl(" + (i * 360 / 8) + ", 100%, 50%)",
+                color: "black",
+                lineHeight: window.innerHeight / 2 + 'px',
                 textAlign: 'center'
             }
         }));
     }
 
-    flex.sequenceFrom(surfaces);
-
-    var finalRatios = [9, 1];
-    var toggle = false;
-    Engine.on('click', function() {
-        var ratios = toggle ? initialRatios : finalRatios;
-        flex.setRatios(ratios, {curve: 'easeOut', duration: 500});
-        toggle = !toggle;
-    });
-
-    mainContext.add(flex);
+    mainContext.add(grid);
 });
